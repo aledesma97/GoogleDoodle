e7968836132d088571d6fcafea4fd5ad59cd95f9
aledesma97 <63313504+aledesma97@users.noreply.github.com>
2020-05-20 10:58:53 -0700

    Added content to the doodle
    Took code from my pen and added it here

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
-console.log('OK');
+//pikachu timeline
+gsap.timeline({
+  defaults: {
+    duration: 1,
+    repeat: -1,
+   yoyo: true
+}
+})
+.to('.pikachu', {
+y: -40,
+scale: 1.2
+}, '+=1')
+.to('.pikachu', {
+y: 0,
+scale:1,
+ease: "bounce"
+});
 
-gsap.from( '#heart1', {
- 
-    x: -600,
-    fill: 'orange',
-    opacity: 0,
-    duration: 3
-    
-  });
+//pokeball timeline
+gsap.timeline({
+defaults: {
+    duration: 1.5,
+}
+})
+.from('.pokeball' , {
+duration: 4,
+x: -400,
+rotation: -390,
+ease: "elastic.out(1, 1)"
+})
+
+
+// .to('.pokeball' , {
+//   transformOrigin: "50% 100%",
+//   rotation: +20,
+// })
+// .to('.pokeball' , {
+//   transformOrigin: "50% 100%",
+//   rotation: 0
+// }, '-=1')
